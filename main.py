6c17da210a6f6fc4988e6fb5c4a2a8524f893dd2
unknown <arjun.varshney1423@gmail.com>
2023-07-24 02:37:27 +0530

    final data in html format

--- main.py
+++ main.py
@@ -5,29 +5,35 @@ def fetch_article_links(folder_link):
     article_links = []
     page_number = 1
     while True:
-        response = requests.get(f"https://help.zebpay.com{folder_link}/page/{page_number}")
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            articles_on_page = soup.select('section.article-list.c-list a.c-link')
-            if not articles_on_page:
+        try:
+            response = requests.get(f"https://help.zebpay.com{folder_link}/page/{page_number}")
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                articles_on_page = soup.select('section.article-list.c-list a.c-link')
+                if not articles_on_page:
+                    break
+                article_links.extend([a['href'] for a in articles_on_page])
+                page_number += 1
+            else:
+                print(f"Failed to fetch data from {folder_link}/page/{page_number}")
                 break
-            article_links.extend([a['href'] for a in articles_on_page])
-            page_number += 1
-        else:
-            print(f"Failed to fetch data from {folder_link}/page/{page_number}")
-            break
+        except Exception as e:
+            print(f"An error occurred with {e}")
     return article_links
 
 def scrape_and_save_file_data(url, filename):
-    response = requests.get(f"https://help.zebpay.com{url}")
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        file_data = soup.get_text()
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(file_data)
-        print(f"Data from {url} has been scraped and saved in {filename}")
-    else:
-        print(f"Failed to fetch data from {url}")
+    try:
+        response = requests.get(f"https://help.zebpay.com{url}")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            file_data = soup.find('article', class_='article-body')
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(str(file_data))
+            print(f"Data from {url} has been scraped and saved in {filename}")
+        else:
+            print(f"Failed to fetch data from {url}")
+    except Exception as e:
+        print(f"There was an error with {e}")
 
 r = requests.get('https://help.zebpay.com/support/solutions')
 

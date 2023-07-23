c01fa2131645a8598b205ee072a193f8da2cafc6
unknown <arjun.varshney1423@gmail.com>
2023-07-24 01:31:29 +0530

    Data scraped from all the links successfully

--- main.py
+++ main.py
@@ -1,74 +1,53 @@
 import requests
-# import time
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service as ChromeService
 from bs4 import BeautifulSoup
 
-# r = requests.get('https://help.zebpay.com/support/home')
-# with open('index.html', 'w') as f:
-#     f.write(r.text)
-with open('index.html', 'r') as f:
-        html_doc = f.read()
-soup = BeautifulSoup(html_doc, 'html.parser')
-soup.prettify()
+def fetch_article_links(folder_link):
+    article_links = []
+    page_number = 1
+    while True:
+        response = requests.get(f"https://help.zebpay.com{folder_link}/page/{page_number}")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            articles_on_page = soup.select('section.article-list.c-list a.c-link')
+            if not articles_on_page:
+                break
+            article_links.extend([a['href'] for a in articles_on_page])
+            page_number += 1
+        else:
+            print(f"Failed to fetch data from {folder_link}/page/{page_number}")
+            break
+    return article_links
 
-# sections = soup.find_all('section', class_='cs-g article-list')
-
-# # Iterate through each section and extract article links
-# article_links = []
-# for section in sections:
-#     article_links.extend([article_link['href'] for article_link in section.find_all('a', href=True)])
-
-# # Function to scrape data from an article URL and save it to a file
-# def scrape_and_save(url, filename):
-#     service = ChromeService(executable_path='chromedriver.exe')
-#     driver = webdriver.Chrome(service=service)
-
-#     try:
-#         driver.get(f"https://help.zebpay.com{url}")
-#         # Wait for a few seconds to ensure dynamic content is loaded
-#         time.sleep(5)
-
-#         soup = BeautifulSoup(driver.page_source, 'html.parser')
-#         # Modify this part to extract the specific data you want from the article
-#         article_data = soup.get_text()  # Here, we're saving the whole text of the article
-
-#         with open(filename, 'w', encoding='utf-8') as file:
-#             file.write(article_data)
-#         print(f"Data from {url} has been scraped and saved in {filename}")
-#     except Exception as e:
-#         print(f"Failed to fetch data from {url}: {e}")
-#     finally:
-#         driver.quit()
-
-# # Scrape data from each article URL and save it into different files
-# for idx, url in enumerate(article_links, start=1):
-#     filename = f"files/article_{idx}.html"
-#     scrape_and_save(url, filename)
-
-# print(soup.title.string)
-# print(soup.title.name)
-
-sections = soup.select('section.cs-g.article-list ul')
-article_links = []
-for section in sections:
-    article_links.extend([article_link['href'] for article_link in section.select('li.article a[href]')])
-
-# Function to scrape data from an article URL and save it to a file
-def scrape_and_save(url, filename):
+def scrape_and_save_file_data(url, filename):
     response = requests.get(f"https://help.zebpay.com{url}")
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        # Modify this part to extract the specific data you want from the article
-        article_data = soup.get_text()
+        file_data = soup.get_text()
         with open(filename, 'w', encoding='utf-8') as file:
-            file.write(article_data)
+            file.write(file_data)
         print(f"Data from {url} has been scraped and saved in {filename}")
     else:
         print(f"Failed to fetch data from {url}")
 
-# Scrape data from each article URL and save it into different files
-for idx, url in enumerate(article_links, start=1):
-    filename = f"files/article_{idx}.html"
-    scrape_and_save(url, filename)
+r = requests.get('https://help.zebpay.com/support/solutions')
+
+with open('index.html', 'w') as impfile:
+    impfile.write(r.text)
+
+with open('index.html', 'r') as f:
+        html_doc = f.read()
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+soup.prettify()
+
+folder_links = [a['href'] for a in soup.select('section.cs-g.article-list a[title]')]
+folder_links = folder_links[1:]
+
+all_article_links = []
+for folder_link in folder_links:
+    article_links = fetch_article_links(folder_link)
+    all_article_links.extend(article_links)
+
+for idx, url in enumerate(all_article_links, start=1):
+    filename = f"scraped_data/article_{idx}.html"
+    scrape_and_save_file_data(url, filename)
